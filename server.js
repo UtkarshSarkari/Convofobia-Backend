b68b9cfd8597838de09c6a61f40a2fe58132fbf6
Utkarsh Sarkari <86049082+UtkarshSarkari@users.noreply.github.com>
2022-06-12 14:58:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ const server = app.listen(PORT,console.log(`Server started on port ${PORT}`));
 
 if(process.env.NODE.ENV == 'production'){
     app.use(express.static("convofobia/build"));
-    // app.get("*", (req,res)=>{
-    //     res.sendFile(path.resolve(__dirname, 'convofobia', 'build', 'index.html'));
-    // })
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'convofobia', 'build', 'index.html'));
+    })
 }
 
 const io = require("socket.io")(server, {
